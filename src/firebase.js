89534108f626cad433bb86125339686c1fe34625
Santino <101665218+Santinou1@users.noreply.github.com>
2022-10-25 12:02:44 -0300

    clean update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7gOlrLQ-ZeHOMUsOpfzS-J5z62QHNUiU",
+    authDomain: "netflix-clone-b5e6e.firebaseapp.com",
+    projectId: "netflix-clone-b5e6e",
+    storageBucket: "netflix-clone-b5e6e.appspot.com",
+    messagingSenderId: "603394362682",
+    appId: "1:603394362682:web:d2d3b0e72ea1e79cbd5403"
+  };
